afcf57b5a4d816ce935145265e8143b7af885b58
venomdp <35393969+venomdp@users.noreply.github.com>
2018-02-25 03:33:23 -0600

    End of Night
    End of Night Commit

--- financial/urls.py
+++ financial/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+from.models import Customer, Crypto, Stock
 
 urlpatterns = [
-    url(r'^$', views.index, name='post_list'),
+    url(r'^$', views.index, name='mainpage'),
+    url(r'^customer/', views.allCustomers, name='allcustomers')
 ]
--- financial/views.py
+++ financial/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Customer, Stock, Crypto
 
 
 def index(request):
-    customers = Customer.objects.all
-    return render(request, 'financial/index.html', {'customers': customers})
+    return render(request, 'financial/index.html')
+
+
+def allCustomers(request):
+    customer = Customer.objects.all
+    return render(request, 'financial/customer.html', {'customer': customer})
+
 
 
